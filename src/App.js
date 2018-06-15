2e450f16c05d8db18532248d5445635f10965396
Desmond <desmond.bera@gmail.com>
2018-06-15 19:01:33 -0400

    clean, unedited timer react app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Clock extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+
+// ReactDOM.render(<Clock />, document.getElementById('root') );
+
+
+// setInterval(tick, 1000);
+export default Clock;
